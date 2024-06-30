010fc32781cbb3ea0edf8078f1fd1f2905c292b9
Israel Ogwu <57731260+israelo19@users.noreply.github.com>
2024-06-29 22:50:09 -0400

    Update
    Added create password function, and load password

--- main.py
+++ main.py
@@ -16,8 +16,25 @@ class PasswordManager:
             
     def load_key(self, path):
         with open(path, 'rb') as f:
-            self.key = f.read()        
+            self.key = f.read()
+            
+    #function for creating the password file
+    
+    def create_password_file(self, path, initial_values = None):        
+        self.password_file = path
         
+        if initial_values is not None:
+            for key, value in initial_values.items():
+                pass # TODO: add password function ()
+            
+    def load_password(self, path):
+        self.password_file = path
+        
+        with open(path, 'r') as f:
+            for line in f:
+                site, encrypted = line.split(":")
+
+                        
 pm = PasswordManager()
 pm.create_key("PasswordManager/mykey.key")
 
