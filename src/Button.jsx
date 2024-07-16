b8fe1f978edc8bcf7ca330208990dac176b08393
Cedric Pierre-Louis <143973071+cedhere@users.noreply.github.com>
2024-07-16 14:26:54 -0400

    Delete src directory

--- src/Button.jsx
+++
@@ -1,7 +0,0 @@
-import './style.css';
-function Button(props){
-    return(
-        <button className = "frontButton">{props.text}</button>
-    );
-}
-export default Button
--- src/Header.jsx
+++
@@ -1,9 +0,0 @@
-function Header(){
-    return(
-        <header>
-            <h1 className = "hacker-text">Secure Password Manager</h1>
-        </header>
-    );
-}
-
-export default Header
